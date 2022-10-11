d0c6fbdd425d17ffdb2857e56ec98fbf561a00f5
Mihail Vorobyev <1viklover@gmail.com>
2022-10-11 15:21:06 +0500

    Initial commit

---
+++ src/main/java/ru/team2/skud/SkudApplication.java
@@ -0,0 +1,13 @@
+package ru.team2.skud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/team2/skud/SkudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.team2.skud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
