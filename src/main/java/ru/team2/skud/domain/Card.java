107647fafecc678d9c40dad6987331f5e50db20c
Mihail Vorobyev <1viklover@gmail.com>
2022-10-19 22:06:01 +0500

    Create a Card dataclass and update migrations

---
+++ src/main/java/ru/team2/skud/domain/Card.java
@@ -0,0 +1,16 @@
+package ru.team2.skud.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("card")
+public class Card {
+    @Id
+    private Long id;
+}
--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,6 +1,36 @@
+
+CREATE TABLE card (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL
+);
+
 CREATE TABLE event (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    card_id INTEGER,
+    card_id INTEGER NOT NULL,
     date INTEGER,
-    type varchar(64)
+    event_type varchar(64) NOT NULL,
+    FOREIGN KEY (card_id) references card (id) on delete CASCADE
 );
+
+CREATE TABLE student (
+    id VARCHAR(8) PRIMARY KEY NOT NULL,
+    card_id INTEGER,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    FOREIGN KEY (card_id) references card (id) on delete SET NULL
+);
+
+CREATE TABLE parent (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    student_id VARCHAR(8) NOT NULL,
+    telephone_number VARCHAR(12) NOT NULL,
+    FOREIGN KEY (student_id) references student (id) on delete CASCADE
+);
+
+CREATE TABLE notification_settings (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    parent_id INTEGER NOT NULL,
+    FOREIGN KEY (parent_id) references parent (id) on delete CASCADE
+);
+
