b8fbf0f766e49d930a8656936fb85081e46629f9
Mihail Vorobyev <1viklover@gmail.com>
2022-10-25 20:12:10 +0500

    Creating WebSecurityConfig Bean with CORS

---
+++ src/main/java/ru/team2/skud/config/WebSecurityConfig.java
@@ -0,0 +1,17 @@
+package ru.team2.skud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        return http.cors().and().csrf(c -> c.disable()).build();
+    }
+}
