732c6cf5c26541cd99ba94584d401b372099d567
Mihail Vorobyev <1viklover@gmail.com>
2022-10-28 01:05:15 +0500

    Upload StudentsMapper for previous commit

--- src/main/java/ru/team2/skud/mapper/StudentMapper.java
+++ src/main/java/ru/team2/skud/mapper/StudentMapper.java
@@ -1,8 +1,17 @@
 package ru.team2.skud.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ru.team2.skud.model.Student;
+import ru.team2.skud.rest.api.NewStudentResource;
 
 @Mapper(componentModel = "spring")
-public interface StudentMapper {
+public abstract class StudentMapper {
 
+    public abstract Student toResource(Student item);
+
+    @Mapping(target = "id")
+    @Mapping(target = "firstName")
+    @Mapping(target = "lastName")
+    public abstract Student toModel(NewStudentResource item);
 }
