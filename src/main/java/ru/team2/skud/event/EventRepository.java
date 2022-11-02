a4af550874eeceaf4a50854206515312b88ce3a3
Mihail Vorobyev <1viklover@gmail.com>
2022-11-02 20:34:04 +0500

    Change the extending interface of EventRepository to ReactiveSortingRepository

--- src/main/java/ru/team2/skud/event/EventRepository.java
+++ src/main/java/ru/team2/skud/event/EventRepository.java
@@ -1,12 +1,8 @@
 package ru.team2.skud.event;
 
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
-import ru.team2.skud.event.Event;
 
 @Repository
-public interface EventRepository extends ReactiveCrudRepository<Event, Long> {
-    Flux<Event> findAll(Sort sort);
+public interface EventRepository extends ReactiveSortingRepository<Event, Long> {
 }
